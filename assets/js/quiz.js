0b6089ee500d1f03072d417a26b6e89b08645f30
Tim Primmer <timprimmer@gmail.com>
2021-06-04 12:51:39 -0500

    added local storage saving and loading

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -11,9 +11,12 @@ var initialsInput = document.querySelector("#initialsinput");
 var initialsText = document.querySelector("#initialstext");
 
 var hsBox = document.querySelector("#hsbox");
+var hsList = document.querySelector("#hslist");
 
 var counter = 0;
 var score = 0;
+var totalScores = [];
+var started = false;
 
 var questions = [ // giant question array that holds the question, potential answers, and the answer itself
   {
@@ -41,6 +44,7 @@ var questions = [ // giant question array that holds the question, potential ans
 var startQuiz = function (event) {
   startText.style.display = "none"; // hiding the text we see at the start of the quiz
   startButton.style.display = "none"; // hiding the Start Quiz button we see at the start of the quiz
+  started = true;
   displayQuestion(counter);
 };
 
@@ -88,7 +92,8 @@ var bodyClicked = function (event) {
       }
     }
   }
-  if (clicked.id === "initialsbutton") // only runs if the thing we are clicking is a button with the "answerid" attribute
+
+  if (clicked.id === "initialsbutton") // runs when we click on the "submit" button for the highscores
   {
     if (initialsinput.value === "") { // checking to see if the user inputted anything
       alert("Please enter in something.");
@@ -99,10 +104,63 @@ var bodyClicked = function (event) {
     else {
       startText.style.display = "none"; // hiding the score text
       initialsBox.style.display = "none"; // hiding the intials box div
-      displayHighscores(initialsinput.value);
+      started = false; // resetting our started status so after we clicked submit, the user can see the highscores page again
+      var tempscore = {
+        initials: initialsinput.value,
+        playerscore: score
+      };
+      totalScores.push(tempscore);
+      saveScores();
+      displayHighscores();
     }
   }
+
+  if (clicked.id === "hsback")  // only runs if we're clicking on the "Go back" button on the highscores page
+  {
+    hsBox.style.display = "none"; // Hiding the highscores
+    hsList.innerHTML = ""; // reset/delete the highscores on the page when we leave
+    resetQuiz();
+  }
+
+  if (clicked.id === "hscore")  // only runs if we're clicking on the "view high scores" button in the top left
+  { 
+    if (started === false)
+    {
+      displayHighscores();
+    }
+    else
+    {
+      alert("Finish the current quiz to see the highscores");
+    }
+  }
+
+  if (clicked.id === "hsclear")  // only runs if we're clicking on the "clear high score" button on the highscores page
+  {
+    totalScores = []; // clearing our local scores
+    saveScores(); // saving the scores (or lack thereof) to the storage
+    displayHighscores();
+  }
 };
+
+var resetQuiz = function () {
+  counter = 0;
+  startText.style.display = "block"; // unhiding the text we see at the start of the quiz
+  startButton.style.display = "block";
+  startText.style.textAlign = "center";
+  startText.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
+  questionText.textContent = "Coding Quiz Challenge";
+  questionText.style.textAlign = "center";
+}
+
+var saveScores = function () {
+  localStorage.setItem("highscores", JSON.stringify(totalScores));
+}
+
+var loadScores = function () {
+  if (localStorage.getItem("highscores")) { // checks if we have anything saved by seeing if our local storage for highscores is empty
+    totalScores = JSON.parse(localStorage.getItem("highscores"));
+  }
+}
 
 var answeredStyle = function (result) {
   var tempDiv;
@@ -131,21 +189,33 @@ var answeredStyle = function (result) {
 
 var quizEnding = function () {
   questionText.textContent = "All done!";
-
   startText.style.display = "block"; // displays the final score text
-
   initialsBox.style.display = "flex"; // displays the intial box where we get the users input
-
   startText.style.textAlign = "left";
   startText.textContent = "Your final score is " + score + ".";
 }
 
-var displayHighscores = function (highscore) {
+var displayHighscores = function () {
+  hsList.innerHTML = "";
+  startText.style.display = "none";
+  startButton.style.display = "none";
   questionText.textContent = "High scores";
+  questionText.style.textAlign = "left";
   hsBox.style.display = "flex"; //unhiding the highscores box
-  console.log(highscore);
+  var tempUl = document.createElement("ul");
+  loadScores();
+  for (x = 0; x < totalScores.length; x++) {
+    var tempLi = document.createElement("li");
+    tempLi.textContent = (x + 1) + ". " + totalScores[x].initials + " - " + totalScores[x].playerscore;
+    tempUl.appendChild(tempLi);
+  }
+  hsList.appendChild(tempUl);
+}
+
+var saveLocalScores = function () {
+ alert("test"); 
 }
 
 startButton.addEventListener("click", startQuiz); // runs when we click on "Start Quiz"
 
-quizDiv.addEventListener("click", bodyClicked); // runs when we click on any of the answers in the main button holder div
+document.addEventListener("click", bodyClicked); // runs when we click on any of the answers in the main button holder div
